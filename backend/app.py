bc36b689d1e3f70e840858935808ec27d3990b95
olilari <larissa.paula.oliveira03@escola.pr.gov.br>
2026-03-10 11:21:22 -0300

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,47 +1,55 @@
 import os
 from flask import Flask, send_from_directory
+import openpyxl
+from datetime import (
+   datetime,
+)
+app = Flask(__name__)
 
-# Caminho base do projeto (uma pasta acima do backend)
-BASE_DIR = os.path.abspaht(os.path.join(os.path.dirname(__file__), ".."))
-
-# Pasta frontend (HTML e JS)
-FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
-
-# Pasta static (CSS)
-STATIC_DIR = os.path.join(BASE_DIR, "static")
-
-app = Flask(__name__, static_folder=STATIC_DIR,static_url_path="/"+ STATIC_DIR)
+# Caminho base do projeto (uma pastacina do beckend)
+#BASE_DIR = os.path.asbpath(os.path.join(os.path.dirname(__file__), '..'))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 
-# ====================================
-# ROTA PRINCIPAL (HTML)
+# Pasta frontend (HTML, JS)
+FRONTEND_DIR = os.path.join(BASE_DIR, "FrontEnd")
+
+#Pasta static (CSS)
+STATIC_DIR= os.path.join(BASE_DIR, "static")
+
+DB_DIR = os.path.join (os.path.dirname(__file__), "..", "db")
+EXCEL_FILE = os.path.join (DB_DIR, "cliente.xlsx")
+
+COLUMNS = [
+   "ID",
+   "Nome",
+   "CPF",
+   "Email",
+   "Telefone",
+   "Endereço",
+   "Observação",
+   "Data Cadastro"
+
+]
 
 
+app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/" + STATIC_DIR)
 
 @app.route("/")
 def home():
-    return send_from_directory(FRONTEND_DIR,"index.html")
+    return send_from_directory(FRONTEND_DIR, "index.html")
 
-
-#Página de Consulta
 @app.route("/consulta")
 def consulta_page():
     return send_from_directory(FRONTEND_DIR, "consulta.html")
 
-
-#Página de Alteração 
 @app.route("/alterar")
 def alterar_page():
-    return send_from_directory(FRONTEND_DIR, "alterar.html")
+ return send_from_directory (FRONTEND_DIR, "alterar.html")
 
 
-if __name__ == "_main_":
-    print("BASE_DIR:", BASE_DIR)
-    print("FRONTEND_DIR:", FRONTEND_DIR)
-    print("STATIC_DIR:", STATIC_DIR)
+if __name__ == "__main__":
+    print("BASE_DIR", BASE_DIR)
+    print("FRONTEND_DIR", FRONTEND_DIR)
+    print("STATIC_DIR", STATIC_DIR)
     app.run(debug=True)
-
-
-
- 
-app.run()
