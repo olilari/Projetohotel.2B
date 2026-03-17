b97971d1736cd3f59ed9f00d8d63f013a4092709
olilari <larissa.paula.oliveira03@escola.pr.gov.br>
2026-03-17 11:23:24 -0300

    Add files via upload

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const formCadastro = document.getElementyById("formCadastro");
+  
 
-    formCadastro.addEventListener("submit", function (e) {
+    if (formCadastro) {
+
+    formCadastro.addEventListener("submit", async (e) =>{
         e.preventDefault();
 
 
@@ -9,11 +12,26 @@ document.addEventListener("DOMContentLoaded", function () {
             new FormData(formCadastro)
         );
 
+        try{
+
+         const resp = await fetch('/api/cadastrar', {
+            method: 'POST',
+            headers: (  'Content-Type': 'apllication/json' },
+
+
+
+         )
+    
+        } catch (err){
+
+            alert('Erro de comunicação com o servidor: ' + err);
+        }
+
         console.log("Dados captutados:");
         console.log("Nome:", dados.nome);
         console.log("Email:", dados,email);
         console.log("Telefone:", dados.telefone);
         console.log(dados);
     });
-
+    }
 });
