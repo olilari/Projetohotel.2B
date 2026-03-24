5bfd2bcb6c6fb88cbf278354da80557b85b784fe
olilari <larissa.paula.oliveira03@escola.pr.gov.br>
2026-03-24 09:26:32 -0300

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,132 +1,144 @@
 import os
-from flask import Flask, send_from_directory, jsonify, request, field
-import openpyxl
+from flask import Flask, send_from_directory, request, jsonify
+import openpyxl # Para ver e editar arquivos .xlsx
 from datetime import (
-   datetime,
+    datetime,
 )
-app = Flask(__name__)
-
-
+# Caminho base do projeto (uma pastacima do backend)
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-
 # Pasta frontend (HTML, JS)
-FRONTEND_DIR = os.path.join(BASE_DIR, "FrontEnd")
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 
-#Pasta static (CSS)
-STATIC_DIR= os.path.join(BASE_DIR, "static")
+# Pasta static (CSS)
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
-DB_DIR = os.path.join (os.path.dirname(__file__), "..", "db")
-EXCEL_FILE = os.path.join (DB_DIR, "clientes.xlsx")
+# Pasta db (Banco de Dados)
+DB_DIR = os.path.join(BASE_DIR, "db")
+EXCEL_FILE = os.path.join(DB_DIR, "clientes.xlsx")
 
-COLUMNS = [
-   "ID",
-   "Nome",
-   "CPF",
-   "Email",
-   "Telefone",
-   "Endereço",
-   "Observações",
-   "Data Cadastro"
+# Pasta do JS
+JS_DIR = os.path.join(BASE_DIR, "frontend", "js")
+JS_FILE = os.path.join(JS_DIR, "main.js")
 
+# Cabecalhos das colunas do Excel (linha 1)
+COLUNAS = [
+    "ID",
+    "Nome",
+    "CPF",
+    "Email",
+    "Telefone",
+    "Endereço",
+    "Observações",
+    "Data Cadastro"
 ]
 
 def init_excel():
-   if not os.path.exists(DB_DIR):
-      os.makedirs(DB_DIR)
+    if not os.path.exists(DB_DIR):
+        os.makedirs(DB_DIR) # Cria a pasta se não existir
+    
+    if not os.path.exists(EXCEL_FILE):
+        workbook = openpyxl.Workbook() # Cria a planilha
+        sheet = workbook.active # Pega a planilha ativa
+        sheet.title = "Clientes" # Nomeia a aba principal
+        sheet.append(COLUNAS) # Adiciona os títulos das colunas
+        workbook.save(EXCEL_FILE) # Salva o arquivo Excel
 
+app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/static")
 
-   if not os.path.exists(EXCEL_FILE):
-      workbook = openpyxl.Workbook()
-      sheet = workbook.active
-      sheet.title = "Clientes"
-      sheet.append(COLUMNS)
-      workbook.save(EXCEL_FILE)
-
-
-app = Flask(__name__, static_folder=STATIC_DIR, static_url_path="/" + STATIC_DIR)
-
+# Pagina principal
 @app.route("/")
 def home():
+    # return "Bom dia galera 2B!"
     return send_from_directory(FRONTEND_DIR, "index.html")
 
+# Pagina de consulta
 @app.route("/consulta")
 def consulta_page():
-    return send_from_directory(FRONTEND_DIR, "consulta.html")
+    return send_from_directory(FRONTEND_DIR, "consultar.html")
 
+# Pagina de alteracao
 @app.route("/alterar")
 def alterar_page():
- return send_from_directory (FRONTEND_DIR, "alterar.html")
+    return send_from_directory(FRONTEND_DIR, "alterar.html")
 
+# Rota para servir imagens, sripts ou outros arquivos na pasta "assets"
 @app.route("/assets/<path:filename>")
-def assets(filname) : 
-   return send_from_directory("../frontend/assets", filname)
+def assets(filename):
+    return send_from_directory("../frontend/assets", filename)
 
+# -------------------------------------------------------------------------------------------------
+# CADASTRAR CLIENTE
+# -------------------------------------------------------------------------------------------------
 @app.route("/cadastrar", methods=["POST"])
 def cadastrar_cliente():
+    """
+    Recebe os dados do formulário (em JSON), valida e salva um novo cliente
+    """
+    try:
+        data = request.json # Dados enviados do frontend via POST (JSON)
 
+        # Capmos obrigatórios que o usuário deve preencher
+        required_fields = ["nome", "cpf", "email", "telefone", "endereco"]
+        if not all(field in data and data[field] for field in required_fields):
+            return (
+                jsonify(
+                    {
+                        "status": "error",
+                        "message": "Todos os campos obrigatórios devem ser preenchidos."
+                    }
+                ),
+                400,
+            )
+        workbook = openpyxl.load_workbook(EXCEL_FILE) # Abre o arquivo Excel
+        sheet = workbook.active
 
-   try:
-      data = request.json
+        # Cria um ID automático (último ID + 1)
+        last_id = 0
+        if sheet.max_row > 1:
+            last_id = sheet.cell(row=sheet.max_row, column=1).value or 0
+        new_id = last_id + 1
 
+        # Cria uma nova linha com os dados informados
+        novo_cliente = [
+            new_id,
+            data.get("nome"),
+            data.get("cpf"),
+            data.get("email"),
+            data.get("telefone"),
+            data.get("endereco"),
+            data.get("observacoes", ""), # Campo opcional
+            datetime.now().strftime("%Y-%m-%d") # Data atual
+        ]
 
-      required_fields = ["nome", "cpf", "email", "telefone", "endereço"]
-      if not all(field in data and data[field] in required_fields):
-         return(
+        sheet.append(novo_cliente) # Adiciona nova linha no Excel
+        workbook.save(EXCEL_FILE) # Salva alterações
+
+        # Retorna mensagem de sucesso
+        return (
             jsonify(
-               {
-                  "status": "error",
-                  "message": "Todos os campos obrigatórios devem ser preenchidos.",
-               }
+                {
+                "status": "sucess",
+                "message": "Cliente cadastrado com sucesso!",
+                "id": new_id
+                }
             ),
-            400,
-         )
-      workbook = openpyxl.load_workbook(EXCEL_FILE)
-      sheet = workbook.active
-
-      last_id = 0 
-      if sheet.max_row > 1:
-         last_id = sheet.cell(row=sheet.max_row, column=1).value or 0 
-         new_id = last_id + 1 
-
-
-      novo_cliente = [
-         new_id,
-         data.get("nome"),
-         data.get("cpf"),
-         data.get("email"),
-         data.get("telefone"),
-         data.get("endereço"),
-         data.get("observação", ""),
-         datetime.now().strftime("%Y-%m-%d"),
-      ]
- 
-      sheet.append(novo_cliente)
-      workbook.save(EXCEL_FILE)
-
-
-      return (
-         jsonify(
-            {
-               "status": "sucess",
-               "message": "Cliente cadastrado com sucesso!",
-               "id": new_id,
-
-            }
-         ),
-         201,
-      )
-   except Exception as e:
-
-      return (
-         jsonify({"status": "error", "message": f"Erro ao salvar no servidor: {e}"}),
-         500,
-      )
-
+            201,
+        )
+    except Exception as e:
+        # Tratamento deerro genérico
+        return (
+            jsonify ({"status": "error", "message": f"Erro ao salvar no servidor: {e}"}),
+            500,
+        )
 
 if __name__ == "__main__":
-    print("BASE_DIR", BASE_DIR)
-    print("FRONTEND_DIR", FRONTEND_DIR)
-    print("STATIC_DIR", STATIC_DIR)
+    print("Base: ", BASE_DIR)
+    print("Front: ", FRONTEND_DIR)
+    print("Static:", STATIC_DIR)
+    print("DB:", DB_DIR)
+    print("Excel:", EXCEL_FILE)
+    print("Pasta JS:", JS_DIR)
+    print("JS:", JS_FILE)
     init_excel()
     app.run(debug=True)
